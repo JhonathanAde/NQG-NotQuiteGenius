1d9c2938c7ba594552824687dac32d658fd8862b
Maximos Salzman <maximossalzman@gmail.com>
2020-12-28 18:49:42 -0500

    added top artists container

--- react-app/src/components/home/SongTile.js
+++ react-app/src/components/home/SongTile.js
@@ -3,7 +3,10 @@ import React from 'react';
 const SongTile = () => {
     return (
         <div className="song-tile">
-            Song Title
+            <div className="song-number">1.</div>
+            <div className="song-image">Image</div>
+            <div className="song-title">Title</div>
+            <div className="song-artist">Artist</div>
         </div>
     );
 };
