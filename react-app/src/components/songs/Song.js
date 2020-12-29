941159235612e833a7b42a7542d637ade0297538
Scott Smith <scott.sncsmith@gmail.com>
2020-12-29 08:49:55 -0800

    Song layout rough

--- react-app/src/components/songs/Song.js
+++ react-app/src/components/songs/Song.js
@@ -4,17 +4,32 @@ import './Song.css';
 
 
 const Song = () => {
-  const [annotation, setAnnotation] = useState('');
+  const [annotation, setAnnotation] = useState("");
 
   useEffect(() => {
     if (annotation === '') {
       return
     }
-    // Need to fetch new annotation if it changed
+
   }, [annotation]);
 
+  const onAnnotationClick = (e) => {
+    const elementToReset = document.querySelector('.annotation-key.active');
+    if (elementToReset) elementToReset.classList.remove('active');
+    if (e.target.classList.contains('annotation-key')) {
+      e.target.classList.add('active')
+      const annotationKey = e.target.innerHTML;
+      // TODO fetch actual annotation and set it
+      document.querySelector('.songpage-annotation-text').innerHTML = annotationKey
+      setAnnotation(annotationKey)
+    }
+    else {
+      setAnnotation("")
+    }
+  }
+
   return (
-    <div className="songpage">
+    <div className="songpage" onClick={onAnnotationClick}>
       <header className="songpage-header">
         <img className="songpage-image" alt="Album Cover"/>
         <div className="songpage-info">
@@ -24,13 +39,20 @@ const Song = () => {
       </header>
       <div className="songpage-content">
         <section className="songpage-lyrics">
-          <p><span className="annotation-key active">Lyrics will go here based of DB information</span><br />
-          <span className="annotation-key ">Second line</span> of text<br />
-          Third line
+          <p><span className="annotation-key" >Lyrics will go here</span> based of DB information<br />
+          <span className="annotation-key">Second line</span> of text<br />
+          Third line<br />
+          <span className="annotation-key">A really long fourth line of lyrics that may lap to two lines in a narrow window and show how the functionality will work with that.</span><br />
+          Fifth line<br />
+          Sixth line<br />
+          <span className="annotation-key">Seventh</span> line<br />
           </p>
         </section>
         <section className="songpage-sidebar">
-          <div className={`songpage-annotation ${(!annotation ? " active" : "")}`}>Some active annotation comment</div>
+          <div className={`songpage-annotation ${(annotation ? " active" : "")}`}>Some active annotation comment
+          <p className="songpage-annotation-text">
+          </p>
+          </div>
           <div className="songpage-sidelinks">
             <NavLink to="/songs/1">Song 1</NavLink>
             <NavLink to="/songs/2">Song 2</NavLink>
