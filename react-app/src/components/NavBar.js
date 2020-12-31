f6af2563de262fdcb1831de828098d27e0eaab4f
Maximos Salzman <maximossalzman@gmail.com>
2020-12-31 00:00:39 -0500

    initial styling

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 import './NavBar.css'
 
 const NavBar = ({ setAuthenticated, authenticated, setUser }) => {
+
+  let history = useHistory()
+  const rerouteHome = () => {
+    history.push("/")
+  }
+
+  const rerouteRegister = () => {
+    history.push("/sign-up")
+  }
+
+  const rerouteCreate = () => {
+    history.push("/create-song")
+  }
   return (
     <nav className="nav-bar">
       <div className="logo">
-          <NavLink to="/" exact={true} activeClassName="active">
-            NQG
-          </NavLink>
+        <div className="home-link" onClick={rerouteHome}>
+          NQG
+        </div>
       </div>
       <div className="search-container">
         <input type="search" className="search-bar" placeholder="search" />
@@ -18,15 +31,13 @@ const NavBar = ({ setAuthenticated, authenticated, setUser }) => {
         {authenticated? 
           <>
             <LogoutButton setAuthenticated={setAuthenticated} setUser={setUser}/>
-            <NavLink to="/create-song" exact={true} activeClassName="active">
-              Create Song
-            </NavLink>
+            <button onClick={rerouteCreate}>
+              CREATE SONG
+            </button>
           </>
           : 
           <>
-            <NavLink to="/sign-up" exact={true} activeClassName="active">
-              Register
-            </NavLink>
+            <button onClick={rerouteRegister}>Register</button>
             {/* <NavLink to="/sign-up" exact={true} activeClassName="active">
               Sign Up
             </NavLink> */}
--- react-app/src/components/home/SongTile.js
+++ react-app/src/components/home/SongTile.js
@@ -13,16 +13,21 @@ const SongTile = ({song, idx}) => {
     }
 
     return (
-        <div className="song-tile">
-            <div className="song-link" onClick={songReroute}>
-                <div className="song-number">{idx}</div>
-                <img className="song-image" src={song.image} alt="album cover"/>
-                <div className="song-title">{song.title}</div>
+        <>
+            <div className="song-tile">
+                <div className="song-link" onClick={songReroute}>
+                    <div className="song-number">{idx}</div>
+                    <img className="song-image" src={song.image} alt="album cover"/>
+                    <div className="song-title">{song.title}</div>
+                </div>
+                <div className="artist-link" onClick={artistReroute}>
+                    <div className="song-artist">{song.artist.name}</div>
+                </div>
             </div>
-            <div className="artist-link" onClick={artistReroute}>
-                <div className="song-artist">{song.artist.name}</div>
+            <div className="divider-container">
+                <div className="divider"></div>
             </div>
-        </div>
+        </>
     );
 };
 
