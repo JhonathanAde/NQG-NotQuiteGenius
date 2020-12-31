7608b274d23ef6488600fdf23dc7ea845d9e0f90
Jhonathan Ade <ade.jhonathan24@gmail.com>
2020-12-31 11:34:43 -0500

    working changes to profile

--- react-app/src/components/profile/AnnotationList.js
+++ react-app/src/components/profile/AnnotationList.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import { Navlink, Link, useParams } from 'react-router-dom';
+import { NavLink, Link, useParams } from 'react-router-dom';
+import ReactHtmlParser from 'react-html-parser';
 import "./AnnotationList.css"
 
 const AnnotationList = ({annoInfo, idx}) => {
@@ -11,12 +12,14 @@ const AnnotationList = ({annoInfo, idx}) => {
   return(
     <div className="annoblock">
       <div className="annoblock-header">
-      <h3 className="annoblock-header_title">{song.title}</h3>
+      <h3 className="annoblock-header_title">
+        <NavLink to={`/songs/${song.id}`}>{song.title}</NavLink>
+        </h3>
       <h7 className="annoblock-header_artist">{artist.name}</h7>
       </div>
       <div className="annoblock-content">
       <p className="annotation-lyrickey_header">Lyrics:</p>
-      <p className="annotation-lyrickey">{lyricKey}</p>
+      <p className="annotation-lyrickey">{ReactHtmlParser(lyricKey)}</p>
       <div>
       <p className="annotation-header">Annotation</p>
       <p className="annotation-content">"{content}"</p>
