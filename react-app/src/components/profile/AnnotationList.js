f8917965b24dec8cda0da649a0cc95fbf369acd3
Jhonathan Ade <ade.jhonathan24@gmail.com>
2021-01-03 15:10:51 -0500

    Commit changes to main

--- react-app/src/components/profile/AnnotationList.js
+++ react-app/src/components/profile/AnnotationList.js
@@ -1,21 +1,31 @@
-import React, {useState, useEffect} from 'react';
-import { NavLink, Link, useParams } from 'react-router-dom';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import ReactHtmlParser from 'react-html-parser';
 import "./AnnotationList.css"
 
 const AnnotationList = ({annoInfo, idx}) => {
 
+  let history = useHistory();
+
   const {content, lyricKey, song} = annoInfo
-  console.log(content)
-  console.log(song)
+  
   const {artist} = song
+
+  const songReroute = () => {
+    history.push(`/songs/${song.id}`)
+  }
+  const artistReroute = () => {
+    history.push(`/artists/${artist.id}`)
+  }
   return(
     <div className="annoblock">
       <div className="annoblock-header">
-      <h3 className="annoblock-header_title">
-        <NavLink to={`/songs/${song.id}`}>{song.title}</NavLink>
+        <h3 className="annoblock-header_title" onClick={songReroute}>
+          {song.title}
         </h3>
-      <h7 className="annoblock-header_artist">{artist.name}</h7>
+        <div className="divider">
+        </div>
+      <div className="annoblock-header_artist" onClick={artistReroute}>{artist.name}</div>
       </div>
       <div className="annoblock-content">
       <p className="annotation-lyrickey_header">Lyrics:</p>
